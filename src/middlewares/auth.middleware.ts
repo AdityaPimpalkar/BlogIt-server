8f5299be1bb7a2f7c52ec0a36e55a3d59b3f27d1
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-03-30 23:48:24 +0530

    middlewares

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { SECRET_KEY } from "@config";
+import { HttpException } from "@exceptions/HttpException";
+import { RequestWithUser, TokenData } from "@interfaces/auth.interface";
+import userModel from "@models/users.model";
+
+const authMiddleware = async (
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const Authorization = req.header("Authorization")
+      ? req.header("Authorization").split("Bearer ")[1]
+      : null;
+
+    if (Authorization) {
+      const secretKey: string = SECRET_KEY;
+      const verificationResponse = verify(
+        Authorization,
+        secretKey
+      ) as TokenData;
+      const userId = verificationResponse._id;
+      const findUser = await userModel.findById(userId);
+
+      if (findUser) {
+        const user: TokenData = {
+          _id: findUser._id,
+          firstName: findUser.firstName,
+          lastName: findUser.lastName,
+          fullName: findUser.fullName,
+          email: findUser.email,
+        };
+        req.user = user;
+        next();
+      } else {
+        next(new HttpException(401, "Wrong authentication token"));
+      }
+    } else {
+      next(new HttpException(404, "Authentication token missing"));
+    }
+  } catch (error) {
+    next(new HttpException(401, "Wrong authentication token"));
+  }
+};
+
+export default authMiddleware;
