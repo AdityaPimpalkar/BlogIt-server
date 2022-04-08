359abb4285ba4093da366576a6e21e070f8f1e8c
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-04-08 23:39:26 +0530

    middleware and tests fix

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -5,7 +5,7 @@ import { User, CreateUser } from "../interfaces/users.interface";
 import usersModel from "../models/users.model";
 
 let appServer: Server;
-describe("/users", () => {
+describe("/me", () => {
   beforeEach(() => {
     appServer = server;
   });
@@ -15,24 +15,30 @@ describe("/users", () => {
     await usersModel.remove({});
   });
 
-  it("should return a user by id", async () => {
-    const user: CreateUser = {
-      firstName: "firstName1",
-      lastName: "lastName1",
-      email: "test1@test1.test",
-      password: "password",
-    };
-    const createdUser: User = await usersModel.create({
-      ...user,
-      fullName: "fullName1",
-    });
+  it("should 401 if user is not logged in", async () => {
+    const res = await request(appServer).get("/me");
 
-    const res = await request(appServer)
-      .get("/me")
-      .send({ user: { _id: createdUser._id } });
-
-    console.log(res);
-
-    expect(res.status).toBe(201);
+    expect(res.status).toBe(401);
   });
+
+  // it("should return a user by id", async () => {
+  //   const user: CreateUser = {
+  //     firstName: "firstName1",
+  //     lastName: "lastName1",
+  //     email: "test1@test1.test",
+  //     password: "password",
+  //   };
+  //   const createdUser: User = await usersModel.create({
+  //     ...user,
+  //     fullName: "fullName1",
+  //   });
+
+  //   const res = await request(appServer)
+  //     .get("/me")
+  //     .send({ user: { _id: createdUser._id } });
+
+  //   console.log(res);
+
+  //   expect(res.status).toBe(201);
+  // });
 });
